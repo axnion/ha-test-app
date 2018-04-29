a57b607ef472485b73743001c0ecdbf99b22a44f
Axel Nilsson <an222yp@student.lnu.se>
2018-04-29 17:20:07 +0200

    Fixed rollback script

--- db/tools/rollback.js
+++ db/tools/rollback.js
@@ -1,8 +1,11 @@
+const execute     = require('./../client').execute
+const getClient   = require('./../client').getClient
+
 
 execute(getClient(), "SELECT * FROM version WHERE table_name='item' ALLOW FILTERING")
 .then(function(resuluts) {
     id = results.rows[0].id
     pre_version = results.rows[0].pre_version
 
-    return executeRisky(getClient(), "UPDATE version SET cur_version = " + pre_version + " WHERE id = " + id )    
+    return execute(getClient(), "UPDATE version SET cur_version = " + pre_version + " WHERE id = " + id )
 })
